b3a6503e36f1871aa20012416e33da4263d3557f
Brett Ahn <abu89@naver.com>
2024-03-04 23:45:09 +0900

    [ch 3,4]개발 환경 구축 및 웹 서버 실습 요구 사항 실습 - 3
    + POST 방식으로 회원가입
    + 302 status code 적용
    + 로그인 하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,6 +2,8 @@ package webserver;
 
 import lombok.extern.slf4j.Slf4j;
 import util.HttpParser;
+import util.IOUtils;
+import webserver.db.DataBase;
 import webserver.domain.User;
 import webserver.dto.RequestLineInfo;
 
@@ -9,6 +11,7 @@ import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.util.Map;
+import java.util.Optional;
 
 @Slf4j
 public class RequestHandler extends Thread {
@@ -23,7 +26,8 @@ public class RequestHandler extends Thread {
         log.info("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+        try (InputStream in = connection.getInputStream();
+             OutputStream out = connection.getOutputStream()) {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
             String line = bufferedReader.readLine();
 
@@ -33,13 +37,43 @@ public class RequestHandler extends Thread {
             }
 
             RequestLineInfo requestLineInfo = HttpParser.parseRequestLine(line);
+            String url = getDefaultUrl(requestLineInfo.path());
+            int contentLength = 0;
+            while (!line.equals("")) {
+                log.debug("header : {}", line);
+                line = bufferedReader.readLine();
+                if (line.contains("Content-Length")) {
+                    contentLength = HttpParser.readContentLength(line);
+                }
+            }
+
             if (requestLineInfo.path().startsWith("/user/create")) {
-                Map<String, String> params = HttpParser.parseContents(requestLineInfo.queryString());
+                String body = IOUtils.readData(bufferedReader, contentLength);
+                Map<String, String> params = HttpParser.parseContents(body);
                 User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
                 log.debug("user = {}", user);
+                DataBase.addUser(user);
+                DataOutputStream dataOutputStream = new DataOutputStream(out);
+                response302Header(dataOutputStream, "/index.html");
+            } else if (requestLineInfo.path().startsWith("/user/login")) {
+                String body = IOUtils.readData(bufferedReader, contentLength);
+                Map<String, String> params = HttpParser.parseContents(body);
+                Optional<User> userOptional = DataBase.findUserById(params.get("userId"));
+
+                DataOutputStream dataOutputStream = new DataOutputStream(out);
+                if (userOptional.isPresent()) {
+                    User user = userOptional.get();
+                    if (user.getPassword().equals(params.get("password"))) {
+                        response302WithCookie(dataOutputStream);
+                        return;
+                    }
+                    responseWithResource(dataOutputStream, "/user/login_failed.html");
+                } else {
+                    responseWithResource(dataOutputStream, "/user/login_failed.html");
+                }
             } else {
                 DataOutputStream dos = new DataOutputStream(out);
-                byte[] body = Files.readAllBytes(new File("./webapp" + requestLineInfo.path()).toPath());
+                byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
                 response200Header(dos, body.length);
                 responseBody(dos, body);
             }
@@ -48,12 +82,50 @@ public class RequestHandler extends Thread {
         }
     }
 
+    private String getDefaultUrl(String path) {
+        if (path.equals("/")) {
+            path = "/index.html";
+        }
+        return path;
+    }
+
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response302Header(DataOutputStream dos, String location) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes("Location: " + location + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response302WithCookie(DataOutputStream dos) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes("Set-Cookie: logined=true \r\n");
+            dos.writeBytes("Location: /index.html" + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void responseWithResource(DataOutputStream dos, String resource) {
+        try {
+            byte[] body = Files.readAllBytes(new File("./webapp" + resource).toPath());
+            response200Header(dos, body.length);
+            responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
---
+++ src/main/java/webserver/db/DataBase.java
@@ -0,0 +1,19 @@
+package webserver.db;
+
+import webserver.domain.User;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class DataBase {
+    public static final Map<String, User> users = new HashMap<>();
+
+    public static void addUser(User user) {
+        users.put(user.getUserId(), user);
+    }
+
+    public static Optional<User> findUserById(String userId) {
+        return Optional.of(users.get(userId));
+    }
+}
